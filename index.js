29e83c1186207b2f804cc24afdda668c479a0008
Steve Albonico <salbonico@gmail.com>
2019-03-15 22:24:23 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,74 @@
+let store = {drivers: [], passengers: [], trips: []}
+
+let driverId = 0
+
+class Driver {
+  constructor(name){
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this)
+    }
+    trips() {
+        return store.trips.filter(
+            function(trip) {
+                return trip.driverId === this.id;
+            }.bind(this)
+        );
+    }
+    passengers() {
+      const output = []
+        this.trips().map(function(trip){
+          output.push(trip.passenger())
+        })
+      return output
+    }
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name){
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+    }
+    trips() {
+        return store.trips.filter(
+            function(trip) {
+                return trip.passengerId === this.id;
+            }.bind(this)
+        );
+    }
+    drivers() {
+      const output = []
+        this.trips().map(function(trip){
+          output.push(trip.driver())
+        })
+      return output
+    }
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(driver,passenger){
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+    }
+    passenger() {
+        return store.passengers.find(
+            function(passenger) {
+                return passenger.id === this.passengerId;
+            }.bind(this)
+        );
+    }
+    driver() {
+        return store.drivers.find(
+            function(driver) {
+                return driver.id === this.driverId;
+            }.bind(this)
+        );
+    }
+}
